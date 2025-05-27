befaa2cf56eb4b628b88292188a3bc1e3c95a309
Devonte <devonte@loveholidays.com>
2025-05-27 20:58:23 +0100

    turbo

--- apps/cli/vitest.config.ts
+++ apps/cli/vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: false,
-    environment: 'node'
+    environment: 'node',
+    passWithNoTests: true
   }
 });
